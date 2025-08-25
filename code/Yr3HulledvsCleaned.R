ebdc9cdd5801e52214893ea9dc1dd20c21a40c8f
LeahTreffer <treffer.leah@gmail.com>
2025-08-25 19:50:18 -0400

    KS yr 3 grain with Rep and inter/mono covariants

--- code/Yr3HulledvsCleaned.R
+++ code/Yr3HulledvsCleaned.R
@@ -3,17 +3,28 @@
 # weighed before and after cleaning
 # Jake suggested a R^2 of >0.8 would be an acceptable threshold
 
+library(lme4)
+library(lmerTest)
+library(MuMIn)
+
 ##### KS
 
 grainweights <- read.csv('data/Year 3 hulled vs cleaned - Sheet1.csv')
+grainweights$Rep <- as.factor(grainweights$Rep)
+grainweights$Treatment <- as.factor(grainweights$Treatment)
 
 plot(grainweights$Cleaned.weight, grainweights$Threshed.Weight)
 
 model <- lm(Cleaned.weight~Threshed.Weight, data=grainweights)
 model2 <- lm(Cleaned.weight~G.from.MasterDataSpreadsheet, data=grainweights)
+model3 <- lm(Cleaned.weight~G.from.MasterDataSpreadsheet + Treatment, data=grainweights)
+model4 <-lmer(Cleaned.weight~G.from.MasterDataSpreadsheet + Treatment+ (1|Rep), data=grainweights)
 
 summary(model)
 summary(model2)
+summary(model3)
+summary(model4)
+r.squaredGLMM(model4)
 
 grainweights$Predicted_Dehulled <- predict(model2, newdata = grainweights)
 
